7745f8b572b81fdb7f7d6f343454bcc97199b5cb
zpt <zpt@zpt.com.br>
2024-05-27 16:19:29 -0400

    feat: added the setDB function in the Department and Company classes

--- src/Department.php
+++ src/Department.php
@@ -5,18 +5,34 @@ require_once './User.php';
 
 class Department {
 	private User\User $user;
+    private $db;
 
 	public function __construct() {
 		$this->user = new User\User(); // @todo fixme
 	}
 
     public function getGreaterDepartament() {
-        $db = $this->user->getDb();
-
-        return $db->q('SELECT dep.name FROM department dep \
+        return $this->db->q('SELECT dep.name FROM department dep \
             INNER JOIN user_department ud ON dep.id = ud.department \
             INNER JOIN user u ON u.id = ud.user \
             WHERE dep.employees = (SELECT MAX(employees) FROM department)');
+    }
+
+    public function setDb($db) {
+        if (!$db || $db->isClosed()) {
+            return false;
+        }
+
+        if ($db->debug) {
+            $db->setGeneralLog('on');
+            error_log($db);
+        }
+
+        if ($db->profiling) {
+            $db->setSlowLog('on');
+        }
+
+        $this->db = $db;
     }
 }
 ?>
